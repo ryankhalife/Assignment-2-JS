6fb23e8aa27753fc9b5edb3c2ffd4c6b56b4b035
Ryan Khalife <khaliferyan@gmail.com>
2022-09-17 08:43:02 +0300

    Added basic game rules

---
+++ game.js
@@ -0,0 +1,28 @@
+let gameRunning = false;
+
+window.onload = () => {
+  const game = document.getElementById("game");
+  const boundaries = game.getElementsByClassName("boundary");
+  const start = document.getElementById("start");
+  const end = document.getElementById("end");
+
+  start.addEventListener("mouseenter", () => {
+    gameRunning = true;
+  });
+
+  end.addEventListener("mouseenter", () => {
+    if (gameRunning) {
+      gameRunning = false;
+      console.log("You won");
+    }
+  });
+
+  for (boundary of boundaries) {
+    boundary.addEventListener("mouseenter", () => {
+      if (gameRunning) {
+        gameRunning = false;
+        console.log("You lost");
+      }
+    });
+  }
+};
