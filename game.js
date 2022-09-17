da593f7f473c1ab9996ef9e513a985488f2a4411
Ryan Khalife <khaliferyan@gmail.com>
2022-09-17 21:33:15 +0300

    created login form inside js

--- game.js
+++ game.js
@@ -2,6 +2,27 @@ let gameRunning = false;
 let scoreInt = 0;
 
 window.onload = () => {
+  document.body.style.visibility = "hidden";
+
+  const style = document.createElement("style");
+  style.innerHTML = css;
+  document.head.appendChild(style);
+
+  document.body.insertAdjacentHTML(
+    "afterbegin",
+    `
+  <div id='page-container' class="page-container" style="visibility: visible !important">
+    <div id="form-container" class="form-container">
+      <h1>Login</h1>
+      <input type="text" placeholder="Username">
+      <input type="password" placeholder="Password">
+      <button>Signup instead</button>
+      <button>Submit</button>
+    </div>
+  </div>
+  `
+  );
+
   const game = document.getElementById("game");
   const boundaries = game.getElementsByClassName("boundary");
   const start = document.getElementById("start");
@@ -57,3 +78,53 @@ window.onload = () => {
     });
   }
 };
+
+const css = `
+.page-container{
+  display:flex;
+  justify-content: center;
+}
+.form-container{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+}
+
+.form-container input{
+  width: 250px;
+  height: 25px;
+}
+
+.form-container h1 + input{
+  margin-bottom: 10px;
+}
+
+.form-container input + input{
+  margin-bottom: 5px;
+}
+
+.form-container input + button{
+  margin-bottom: 20px;
+  background: none;
+  border: none;
+}
+
+.form-container input + button:hover{
+  cursor: pointer;
+}
+
+.form-container button + button{
+  border: none;
+  background-color: rgba(128,128,128,0.2);
+  height: 30px;
+  width: 250px;
+  border-radius: 20px;
+}
+
+.form-container button + button:hover{
+  background-color: rgba(128,128,128,0.4);
+  cursor: pointer;
+}
+
+`;
